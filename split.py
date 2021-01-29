fcb89397e9be3a4ba326e403de730604417d081a
anhnct <anhnguyencongtu@admicro.vn>
2021-01-29 20:46:54 +0700

    commit

---
+++ split.py
@@ -0,0 +1,20 @@
+import sys
+
+path_corpus=sys.argv[1]
+path_save=sys.argv[2]
+
+if __name__=='__main__':
+    i=0
+    k=0
+    sentences=[]
+    with open(path_corpus,'r',encoding='utf-8') as corpus:
+        for line in corpus:
+            sentences.append(line)
+            i+=1
+            if i == 5000000:
+                with open(path_save+'/all_text_{}.txt'.format(k),'w',encoding='utf-8') as file:
+                    for sentence in sentences:
+                        file.writelines(sentence)
+            i=0
+            k+=1
+            sentences=[]
